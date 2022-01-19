7e73b090a64d05bd9e8d995ec2573866db3f9e62
andre_henrique_teixeira <andrehenriqueteixeira10@gmail.com>
2022-01-18 21:50:02 -0300

    lançamento do sistema

---
+++ src/main/java/com/loter/prob/lab/loterproblab/LoterProbLabApplication.java
@@ -0,0 +1,13 @@
+package com.loter.prob.lab.loterproblab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoterProbLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoterProbLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loter/prob/lab/loterproblab/LoterProbLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loter.prob.lab.loterproblab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoterProbLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
